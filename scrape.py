86745355bc23f7b879bdbebdb3fdd74a158097c6
Julian <julian.nicholas.weidner@gmail.com>
2024-05-01 13:50:44 +0200

    partially working detail page. Very ugly

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from tournament import Tournament
+from tournament import Tournament, TournamentDetail
 
 #create driver/browser open page
 driver = webdriver.Chrome()
@@ -13,6 +13,7 @@ driver.implicitly_wait(2.0)
 def close_gdpr():
     print('closing GDPR')
     deny_button = driver.find_element(By.CSS_SELECTOR, "button[data-cookiefirst-action='reject']")
+    driver.implicitly_wait(4.0)
     deny_button.click()
     driver.implicitly_wait(2.0)
 
@@ -38,7 +39,11 @@ def get_active_tournaments():
     #figure out a proper return
     return (active_tournaments)
 
+
 def create_tournament_obj(tournament_card):
+
+    detail_id = tournament_card.find_element(By.CSS_SELECTOR, 'a[card-name="buttonInfoTournament"]').get_attribute('href').split("=")[-1]
+
     data = {
         'title': tournament_card.find_element(By.CSS_SELECTOR, 'h3.header-name-tournament').text,
         'team_size': tournament_card.find_element(By.CSS_SELECTOR, "span[card-name='formatTeam']").text,
@@ -46,16 +51,39 @@ def create_tournament_obj(tournament_card):
         'battle_mode': tournament_card.find_element(By.CSS_SELECTOR, "span[card-name='gameMode']").text,
         'tournament_type': tournament_card.find_element(By.CSS_SELECTOR, "span[card-name='typeTournament']").text,
         'region': tournament_card.find_element(By.CSS_SELECTOR, "span[card-name='clusterTournament']").text,
-        'tournament_date': tournament_card.find_element(By.CSS_SELECTOR, "p[card-name='dayTournament']").text
+        'tournament_date': tournament_card.find_element(By.CSS_SELECTOR, "p[card-name='dayTournament']").text,
+        'detail_id': detail_id
     }
 
-    return Tournament(**data )
+    return Tournament(**data)
+
+def get_tournament_details(tournament):
+    url = f'https://tss.warthunder.com/index.php?action=tournament&id={tournament.detail_id}'
+    sub_driver = driver = webdriver.Chrome()
+    sub_driver.get(url)
+    
+    data = {
+        'id': tournament.detail_id,
+        'prize_pool':  driver.find_element(By.CSS_SELECTOR, 'b[id-tss="prize_pool"]').text,
+        #'maps': ,
+        #'nations': , 
+        #'vehicles': , 
+    }
+
+    sub_driver.quit()
+    return data
+
+def create_tournament_detail(data):
+    return TournamentDetail(**data)
+
+    
+
     
 
 def main():
     #close gdpr 
     close_gdpr()
-    driver.implicitly_wait(2.0)
+    
 
     #create list of active tournaments
     active_tournaments = get_active_tournaments()
@@ -63,7 +91,12 @@ def main():
     print('Creating Tournament Objects (printed from main)')
     for tournament in active_tournaments:
         tourn_obj = create_tournament_obj(tournament)
-        print(tourn_obj.title, tourn_obj.date)
+        tourn_detail_elements = get_tournament_details(tourn_obj)
+        tourn_detail_obj = create_tournament_detail(tourn_detail_elements)
+
+        print( tourn_obj.title, tourn_obj.date, "--", tourn_detail_obj.id, tourn_detail_obj.prize_pool )#, tourn_detail_obj.prize_pool )
+    
+    
     driver.quit()
 
 
--- tournament.py
+++ tournament.py
@@ -9,9 +9,23 @@ class Tournament():
         self.tournament_type = kwargs['tournament_type'] #SE (single elim)
         self.region = kwargs['region'] # EU, SEA, NA
         self.date = kwargs['tournament_date'] #May 1, 24, 17:50 UTC - May 1, 24, 19:50 UTC | why yes, this will have to be handled too <3
+        self.detail_id = kwargs['detail_id']
     
     def __str__(self):
         return self.title 
+
+class TournamentDetail(): #through table with references to those other tables? Most likely overkill
+    def __init__(self, **kwargs):
+        self.id = kwargs['id']
+       # self.maps = kwargs['maps'] #own table
+       # self.nations = kwargs['maps'] #own table
+       # self.vehicles = kwargs['vehicles'] #own table
+        self.prize_pool = kwargs['prize_pool']
+
+    def __str__(self):
+        return f"Tournament Detail:  {self.id}"
+
+
 
     
 
